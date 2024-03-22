d85c4aa415e9677361dadac8241bbf1f102b8cb1
thesidex <42481614+thesidex@users.noreply.github.com>
2024-03-21 20:15:07 -0500

    fix AttendanceRepository

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/repository/AttendanceRepository.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/repository/AttendanceRepository.java
@@ -2,6 +2,8 @@ package edu.miu.cs.cs544.repository;
 
 import edu.miu.common.repository.BaseRepository;
 import edu.miu.cs.cs544.domain.Attendance;
+
+import org.checkerframework.checker.units.qual.m;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,7 +16,8 @@ public interface AttendanceRepository extends BaseRepository<Attendance, Long> {
     List<Attendance> findAllRecordsByScannerCode(@Param("code") String code);
     @Query(value = "select a from Attendance a where a.scanner.event.id = :evtId")
     List<Attendance> findAllByEventId(@Param("evtId") String evtId);
-    @Query(value = "select a.* from attendance a, member m, account acc where a.member_id = m.id and m.id = acc.member_id and acc.id = :accountId and format(date_time,'yyyy-mm-dd') between :startDate and :endDate", nativeQuery = true)
+    //@Query(value = "select a.* from attendance a, member m, account acc where a.member_id = m.id and m.id = acc.member_id and acc.id = :accountId and format(date_time,'yyyy-mm-dd') between :startDate and :endDate", nativeQuery = true)
+    @Query(value = "SELECT a.* FROM attendance a JOIN member m ON a.member_id = m.id JOIN account acc ON m.id = acc.member_id WHERE acc.id = :accountId AND a.date_time >= :startDate AND a.date_time < :endDate", nativeQuery = true)
     List<Attendance> findAllByAccountId(@Param("accountId") Long accountId, @Param("startDate") String startDate,@Param("endDate") String endDate);
     @Query(value = "select a from Attendance a where a.member.id = :memId and a.scanner.event.id = :evtId")
     List<Attendance> findAllByMemberIdAndEventId(@Param("memId") long memId, @Param("evtId") long evtId);
